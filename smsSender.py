7c6804d548dbd2f600e684e7458c9c58a743d2b1
Timofey <timtwin2@mail.ru>
2017-12-23 22:47:42 +0300

    sender arg

--- smsSender.py
+++ smsSender.py
@@ -11,8 +11,11 @@ class SMS:
         rqData["login"] = self.login
         rqData["psw"] = self.psw
         r = requests.get(self.url + url, params=rqData)
-        return 'r.text'
+        return r.text
 
-    def sendSMS(self, phone, message):
+    def sendSMS(self, phone, message,sender=''):
         rqData = {"phones": phone, "mes": message, "charset": "utf-8"}
+        if sender != "":
+            rqData['sender'] = sender
         return self.doRequest(rqData, "sys/send.php")
+
