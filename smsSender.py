8dfd537076e5f155b8ff07752ef765eaeb9a28cf
Timofey <timtwin2@mail.ru>
2017-12-23 22:19:45 +0300

    smsSender

---
+++ smsSender.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import requests
+
+class SMS:
+    def __init__(self, l, p):
+        self.login = l
+        self.psw = p
+        self.url = "http://smsc.ru/"
+
+    def doRequest(self, rqData, url):
+        rqData["login"] = self.login
+        rqData["psw"] = self.psw
+        r = requests.get(self.url + url, params=rqData)
+        return 'r.text'
+
+    def sendSMS(self, phone, message):
+        rqData = {"phones": phone, "mes": message, "charset": "utf-8"}
+        return self.doRequest(rqData, "sys/send.php")
